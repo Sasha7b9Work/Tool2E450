7b48b47e18d5a1bdb7187fb92937f53312ef270e
Sasha7b9Work <Sasha7b9@tut.by>
2025-02-09 15:50:30 +0300

    *

--- sources/Device/Objects/main_d.d
+++
@@ -1,3 +0,0 @@
-./objects/main_d.o: src\main_d.cpp src\defines.h src\no-warnings.h \
-  src\Utils\Log.h src\Hardware\HAL\HAL.h src\Hardware\HAL\HAL_PINS.h \
-  src\Hardware\Timer.h
--- sources/Device/Objects/startup_stm32f407xx.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f407xx.o: src\Hardware\CMSIS\startup_stm32f407xx.s
