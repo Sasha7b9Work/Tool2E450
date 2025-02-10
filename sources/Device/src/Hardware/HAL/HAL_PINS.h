95aa20661214452a54e38b5c4e11b2bb8472f810
Sasha7b9Work <Phuntik-it@tut.by>
2025-02-10 15:59:32 +0300

    *

--- sources/Device/src/Hardware/HAL/HAL_PINS.h
+++ sources/Device/src/Hardware/HAL/HAL_PINS.h
@@ -47,75 +47,33 @@ namespace HAL_PINS
     void Init();
 }
 
+extern PinOut pinT3D_PULS_2;
+extern PinOut pinT3D_SIGN_2;
+extern PinIn  pinT3D_D01_2;
+extern PinOut pinT3D_SIGN_1;
+extern PinOut pinT3D_S0N1;
+extern PinOut pinT3D_PULS_1;
+extern PinIn  pinENC_A7_A;
+extern PinIn  pinENC_A7_B;
+extern PinIn  pinT3D_D01_1;
 
-extern PinOut pinHL1;
-extern PinOut pinHL2;
-extern PinOut pinHL3;
-extern PinOut pinHL4;
-
-extern PinOut pinDISP_BL_C;
-extern PinOut pinDISP_RESET;
-extern PinOut pinDISP_WAIT;
-
-extern PinOut pinLED;
-
-extern PinIn pinMPG_X1;
-extern PinIn pinMPG_X10;
-extern PinIn pinMPG_X100;
-extern PinIn pinMPG_X;
-extern PinIn pinMPG_Y;
-extern PinIn pinMPG_A;
-extern PinIn pinMPG_B;
-extern PinIn pinMPG_4;
-extern PinIn pinMPG_5;
-extern PinIn pinMPG_6;
-extern PinIn pinMPG_CN;
-
-extern PinIn pinIN_1;
-extern PinIn pinIN_2;
-extern PinIn pinIN_3;
-extern PinIn pinIN_4;
-extern PinIn pinIN_5;
-extern PinIn pinIN_6;
-extern PinIn pinIN_7;
-extern PinIn pinIN_8;
-extern PinIn pinIN_9;
-extern PinIn pinIN_10;
-extern PinIn pinIN_11;
-extern PinIn pinIN_12;
-extern PinIn pinIN_13;
-extern PinIn pinIN_14;
-extern PinIn pinIN_15;
-extern PinIn pinIN_16;
-extern PinIn pinIN_17;
-extern PinIn pinIN_18;
-extern PinIn pinIN_19;
-extern PinIn pinIN_20;
-extern PinIn pinIN_21;
-extern PinIn pinIN_22;
-extern PinIn pinIN_23;
-extern PinIn pinIN_24;
-extern PinIn pinIN_25;
-extern PinIn pinIN_31;
-
-extern PinIn pinSA1;
-extern PinIn pinSA2_1;
-extern PinIn pinSA2_2;
-extern PinIn pinSA3_1;
-extern PinIn pinSA3_2;
-
-extern PinIn pinSB2;
-extern PinIn pinSB3;
-extern PinIn pinSB4;
-extern PinIn pinSB5;
-extern PinIn pinSB6;
-extern PinIn pinSB7;
-extern PinIn pinSB8;
-extern PinIn pinSB9;
-extern PinIn pinSB10;
-
-extern PinIn pinENC_A7_A;
-extern PinIn pinENC_A7_B;
-extern PinIn pinENC_A8_A;
-extern PinIn pinENC_A8_B;
-
+extern PinOut pinESQ_X1;
+extern PinOut pinESQ_X2;
+extern PinIn  pinESQ_TC;
+extern PinIn  pinRKF;
+extern PinIn  pinKM1;
+extern PinIn  pinKM2;
+extern PinIn  pinKM3;
+extern PinIn  pinKM4;
+extern PinIn  pinQF5;
+extern PinIn  pinQF6;
+extern PinIn  pinQF7;
+extern PinIn  pinQF10;
+extern PinIn  pinQF11;
+extern PinIn  pinQF12;
+extern PinIn  pinQF13;
+extern PinIn  pinQF14;
+extern PinIn  pinQF15;
+extern PinIn  pinKM5;
+extern PinIn  pinKM6;
+extern PinOut
