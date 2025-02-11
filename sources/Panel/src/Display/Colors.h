b71a30bff724c2f7f06373dc72c0f3a51516bf72
Sasha7b9Work <Sasha7b9@tut.by>
2025-02-11 22:01:59 +0300

    *

--- sources/Panel/src/Display/Colors.h
+++ sources/Panel/src/Display/Colors.h
@@ -46,10 +46,10 @@ private:
 
 #ifdef WIN32
 
-    #define MAKE_COLOR(r, g, b)     ((uint)((b) | ((g) << 8) | ((r) << 16)))
+    #define MAKE_COLOR(r, g, b)     ((uint)((r) | ((g) << 8) | ((b) << 16)))
 
-    #define RED_FROM_COLOR(color)   ((uint8)(((color) >> 16) & 0xFF))
+    #define BLUE_FROM_COLOR(color)   ((uint8)(((color) >> 16) & 0xFF))
     #define GREEN_FROM_COLOR(color) ((uint8)(((color) >> 8) & 0xFF))
-    #define BLUE_FROM_COLOR(color)  ((uint8)((color) & 0xFF))
+    #define RED_FROM_COLOR(color)  ((uint8)((color) & 0xFF))
 
 #endif
--- sources/Panel/src/Menu/Pages/PageMain.cpp
+++ sources/Panel/src/Menu/Pages/PageMain.cpp
@@ -1,19 +1,28 @@
 // 2025/02/11 21:43:27 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Menu/Pages/Pages.h"
+#include "Display/Primitives.h"
+
+
+using namespace Primitives;
 
 
 namespace PageMain
 {
+    static void DrawUppderZone()
+    {
+        Rect(800, 100).FillRounded(10, 10, 1, Color::YELLOW, Color::BLACK);
+    }
+
+    static void Func_Draw()
+    {
+        DrawUppderZone();
+    }
+
     static Item *items[] =
     {
         nullptr
     };
-
-    static void Func_Draw()
-    {
-
-    }
 
     static Page page(items, Func_Draw);
 
