95fbddee02cc833fa57baef46feddfa4d79bffec
Sasha7b9Work <Sasha7b9@tut.by>
2025-02-11 21:17:08 +0300

    Выводит пустое окно

--- sources/Panel/src/Display/Display.cpp
+++ sources/Panel/src/Display/Display.cpp
@@ -1,11 +1,22 @@
 // 2025/02/11 20:31:59 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Display/Display.h"
+#include "Display/Font/Font.h"
+
+
+namespace Display
+{
+    void InitHardware();
+}
 
 
 void Display::Init()
 {
+    InitHardware();
 
+    Font::Set(TypeFont::GOSTAU16BOLD);
+
+    Font::SetSpacing(2);
 }
 
 
